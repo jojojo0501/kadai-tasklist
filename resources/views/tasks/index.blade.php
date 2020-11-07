e97dad678959ced0e1e0e4c893b771197c26d2d0
jota.kaneko <sarukiti0418@outlook.jp>
2020-11-07 11:13:24 +0000

    syuusei3

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,7 +18,7 @@
                 <tr>
                     <td>{!! link_to_route("tasks.show",$task->id,["task"=>$task->id])!!}</td>
                     <td>{{$task->content}}</td>
-                    <td>{{$task->title}}</td>
+                    <td>{{$task->status}}</td>
                 </tr>
             </tbody>
                 <?php } ?> 
